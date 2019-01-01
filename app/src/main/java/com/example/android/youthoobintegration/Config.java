7d6fe05414de741c5619342ea0c4f032df47788c
Deepak Sharma <dsdeepak405@gmail.com>
2019-01-01 14:20:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/youthoobintegration/Config.java
@@ -0,0 +1,10 @@
+package com.example.android.youthoobintegration;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAYv_t-eNp3aQUuDe2IkBBZYYxPhwhNCdQ";
+
+}
